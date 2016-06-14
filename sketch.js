1fcf6104acef690475d4116ba11a5bb4bfd958d5
Connor Cassidy <connormcassidy@gmail.com>
2016-06-14 12:42:25 -0400

    Initial commit with some problems with vertical alignment on divs

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var wide = 16;
+var tall = 16;
+
+$(document).ready(function() { 
+    makeGrid(wide, tall);
+});
+
+var makeGrid = function(wide, tall) {
+    for (i=0; i<wide; i++) {
+        for (j=0; j<tall; j++) {
+            $box = $('<div class="box">').appendTo(".sketchpad")
+            $box.css("width", calculateBoxWidth(wide));
+            $box.css("height", calculateBoxHeight(tall));
+        }
+    }
+}
+    
+var calculateBoxWidth = function(wide) {
+    return ($(".sketchpad").width()/wide + "px");
+}
+
+var calculateBoxHeight = function(tall) {
+    return ($(".sketchpad").height()/tall + "px");
+}
